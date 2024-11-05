76ef641785581a1321bc151b74fed4bfe70c6d62
Mohammed Imran Mohsin <39203144+mims1234@users.noreply.github.com>
2024-11-05 10:42:46 +0530

    setting up env and discord base code

---
+++ bot.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+client.login(process.env.TOKEN);
