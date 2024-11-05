210ae9384b1499584ee4d1962ea6f97f9694ced4
Mohammed Imran Mohsin <39203144+mims1234@users.noreply.github.com>
2024-11-05 10:46:29 +0530

    Added MongoDB setup code and Handlers for local commands,slash commands and events

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
 
 const client = new Client({
     intents: [
@@ -9,5 +12,45 @@ const client = new Client({
         GatewayIntentBits.GuildMembers
     ]
 });
+
+client.commands = new Collection();
+client.slashCommands = new Collection();
+client.config = require('./config.json');
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('MongoDB connection error:', err));
+
+// Load Command Handlers
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(path.join(commandsPath, file));
+    client.commands.set(command.name, command);
+}
+
+// Load Slash Commands
+const slashCommandsPath = path.join(__dirname, 'slashCommands');
+const slashCommandFiles = fs.readdirSync(slashCommandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of slashCommandFiles) {
+    const command = require(path.join(slashCommandsPath, file));
+    client.slashCommands.set(command.data.name, command);
+}
+
+// Event Handler
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const event = require(path.join(eventsPath, file));
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
 
 client.login(process.env.TOKEN);
