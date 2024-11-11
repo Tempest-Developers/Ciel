971a5f3e96e694c4d7f5144c6f5246ba6c784971
Mohammed Imran Mohsin <39203144+mims1234@users.noreply.github.com>
2024-11-12 04:20:26 +0530

    messageUpdate fixed

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -5,7 +5,15 @@ const { checkPermissions, checkIfGuildAllowed } = require('../utility/auth');
 module.exports = {
 	name: Events.MessageUpdate,
 	once: false,
-	async execute(client, oldMessage, newMessage) {
+	async execute(oldMessage, newMessage, { database }) {
+        // Check if messages exist and have necessary properties
+        if (!oldMessage?.author || !newMessage?.author) return;
+        if (!oldMessage?.guild || !newMessage?.guild) return;
+
+        // Get client from message
+        const client = oldMessage.client;
+        if (!client) return;
+
 		if (oldMessage.author.id === client.user.id) return;
         if (newMessage.author.id === client.user.id) return;
 
@@ -14,7 +22,6 @@ module.exports = {
 		if (!checkPermissions(oldMessage.channel, client.user)) return;
 
 		if (!(await checkIfGuildAllowed(client, newMessage.guild.id)) || !(await checkIfGuildAllowed(client, oldMessage.guild.id))) return;
-
 
         // Handle Mazoku messages
         handleEditedMazokuMessage(client, oldMessage, newMessage, client.config.mazokuID);
