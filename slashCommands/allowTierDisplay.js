5e5e58cfa21ceaee00c460e5bbe52be79c196683
Mohammed Imran Mohsin <39203144+mims1234@users.noreply.github.com>
2024-11-13 00:54:58 +0530

    allowtier message added

--- slashCommands/allowTierDisplay.js
+++ slashCommands/allowTierDisplay.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('allowtierdisplay')
-        .setDescription('Toggle the High Tier role ping feature for this server')
+        .setDescription('Toggle the tier display feature for this server')
         .setDefaultMemberPermissions(PermissionFlagsBits.ADMINISTRATOR | PermissionFlagsBits.MANAGE_ROLES | PermissionFlagsBits.MANAGE_MESSAGES),
 
     async execute(interaction) {
@@ -40,7 +40,7 @@ module.exports = {
             console.log(interaction.guild.name+" "+newValue)
 
             await interaction.reply({
-                content: `High Tier role ping feature has been ${newValue ? 'enabled' : 'disabled'} for this server.`,
+                content: `Displays tiers preview info ${newValue ? 'enabled' : 'disabled'} for this server.`,
                 ephemeral: true
             });
 
