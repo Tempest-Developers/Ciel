a67cb90dc1756a32f2be5773ff5f3f4405140ea1
Mohammed Imran Mohsin <39203144+mims1234@users.noreply.github.com>
2024-11-16 05:59:04 +0530

    summon and manual one UI change and timer

--- utility/summonHandler.js
+++ utility/summonHandler.js
@@ -198,7 +198,7 @@ async function handleSummonInfo(client, newMessage, newEmbed, messageId) {
             const elapsedTime = Math.floor(Date.now() / 1000) - startTime;
 
             // Calculate countdown time by subtracting the elapsed time from the desired countdown
-            const countdownTime = startTime + 17 - elapsedTime;
+            const countdownTime = startTime + 20 - elapsedTime;
             const nextSummonTime = startTime + 120 - elapsedTime;
 
             // Create base embed with countdown
