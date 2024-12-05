f5c1964c2631844d2bda07365c2492975f2daac1
Mohammed Imran Mohsin <39203144+mims1234@users.noreply.github.com>
2024-12-06 03:31:02 +0530

    wrote free instead of false

--- slashCommands/gate/utils/constants.js
+++ slashCommands/gate/utils/constants.js
@@ -1,8 +1,8 @@
 module.exports = {
     GATE_GUILD: '1240866080985976844',
     SR_PING_ROLE: '1305567492277796908',
-    LEAD_COOLDOWN: 5, // 5 seconds for leads
-    USER_COOLDOWN: 10, // 10 seconds for regular users
+    LEAD_COOLDOWN: 2, // 5 seconds for leads
+    USER_COOLDOWN: 5, // 10 seconds for regular users
     COSTS: {
         TICKET: 50,
         PREMIUM: 500,
@@ -10,5 +10,5 @@ module.exports = {
     },
     MAX_TOKENS: 10000,
     MAX_TOKENS_TICKET: 10,
-    GIVEAWAY_FIRST_TICKET_FREE: free // New toggle for first ticket being free
+    GIVEAWAY_FIRST_TICKET_FREE: false // New toggle for first ticket being free
 };
